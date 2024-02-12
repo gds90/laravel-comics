595cf424dc1cdb00b08de811e42e25d18d2f81ea
gds90 <you@example.com>
2024-02-12 16:59:38 +0100

    Added section buy products before footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -22,6 +22,42 @@
             <p>DC COMICS</p>
         </div>
     </main>
+    <div class="buyProduct">
+        <div class="contentBuyProduct">
+            <ul class="list-unstyled ">
+                <li>
+                    <a>
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}" alt="/">
+                        <span class="label">DIGITAL COMICS</span>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" alt="/">
+                        <span class="label">DC MERCHANDISE</span>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" alt="/">
+                        <span class="label">SUBSCRIPTION</span>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" alt="/">
+                        <span class="label">COMIC SHOP LOCATOR</span>
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" alt="/">
+                        <span class="label">DC POWER VISA</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
     @include('partials.footer')
 </body>
 
