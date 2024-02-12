64d9acce005e526343d97ca73501ce42e601255a
gds90 <you@example.com>
2024-02-12 16:18:34 +0100

    Added a only visual search in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -13,6 +13,10 @@
     <!-- Styles -->
     @vite('resources/js/app.js')
 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
 </head>
 
 <header>
@@ -64,8 +68,8 @@
                     </nav>
                 </div>
             </div>
-            <div class="col-2">
-
+            <div class="col-2 text-center search">
+                <span>Search <i class="fa-solid fa-magnifying-glass"></i></span>
             </div>
         </div>
     </div>
